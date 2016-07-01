e02a34432cf331fdaba8e012ee65806d37e7be8f
Alex Muravya <asm@virtalab.net>
2016-07-01 07:43:40 +0300

    Share Activity

--- app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.*;
 import android.widget.TextView;
 
@@ -39,19 +42,53 @@ public class DetailActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+
     public static class DetailFragment extends Fragment {
+
+        private static final String TAG = DetailFragment.class.getSimpleName();
+
+        private static final String HASHTAG = " #WeatherApp";
+
+        private static String forecastString;
+
         public DetailFragment() {
+            setHasOptionsMenu(true);
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
 
-            String forecast = getActivity().getIntent().getStringExtra(Intent.EXTRA_TEXT);
+            forecastString = getActivity().getIntent().getStringExtra(Intent.EXTRA_TEXT);
 
             TextView detailsText = (TextView) rootView.findViewById(R.id.detail_text);
-            detailsText.setText(forecast);
+            detailsText.setText(forecastString);
             return rootView;
+        }
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            inflater.inflate(R.menu.detail_fragment, menu);
+
+            MenuItem shareItem = menu.findItem(R.id.action_share);
+
+            ShareActionProvider shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
+
+            if(shareActionProvider != null) {
+                shareActionProvider.setShareIntent(createShareForecastIntent());
+            } else {
+                Log.d(TAG, "Share Action Provider is not available");
+            }
+
+            super.onCreateOptionsMenu(menu, inflater);
+        }
+
+        private Intent createShareForecastIntent() {
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_TEXT, forecastString + HASHTAG);
+            return shareIntent;
         }
     }
 }
